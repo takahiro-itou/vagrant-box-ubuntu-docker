ad1a194640a5c0d62ecf7000b9ef735a48a4c663
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2021-12-18 13:05:38 +0900

    パッケージ Docker をインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -20,6 +20,17 @@ sudo  apt  install -y  build-essential                          \
 sudo  apt  install -y  ubuntu-desktop
 sudo  apt  install -y  qemu
 
+# NASM
+sudo  apt  install -y  nasm
+
+# Docker
+sudo  apt  install -y  ca-certificates  curl  gnupg
+curl  -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable" \
+    | sudo tee /etc/apt/sources.list.d/docker.list
+sudo  apt  update
+sudo  apt  install -y  docker-ce
+
 # clean up
 sudo  apt  -y  --purge  autoremove
 sudo  apt  autoclean
